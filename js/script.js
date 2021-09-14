608853338a97115c87ba44b93d0bfab21d135b13
Debabrata Saha <sahadebabrata570@gmail.com>
2021-09-15 05:28:36 +0600

    dynamic icon link found after a decade !! and dynamic icon seted

--- js/script.js
+++ js/script.js
@@ -25,4 +25,35 @@ const displayWeatherInfo = (city) => {
     const daySituation = document.getElementById('day-situation')
     daySituation.innerText = city.weather[0].main;
 
+    const weatherIcon = document.getElementById('weather-icon')
+    weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/11d@2x.png')
+
+    if (city.weather[0].icon == '11d' || city.weather[0].icon == '11n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/11d@2x.png')
+    }
+    else if (city.weather[0].icon == '09d' || city.weather[0].icon == '09n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/09d@2x.png')
+    }
+    else if (city.weather[0].icon == '10d' || city.weather[0].icon == '10n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/10d@2x.png')
+    }
+    else if (city.weather[0].icon == '13d' || city.weather[0].icon == '13n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/13d@2x.png')
+    }
+    else if (city.weather[0].icon == '50d' || city.weather[0].icon == '50n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/50d@2x.png')
+    }
+    else if (city.weather[0].icon == '01d' || city.weather[0].icon == '01n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/01d@2x.png')
+    }
+    else if (city.weather[0].icon == '02d' || city.weather[0].icon == '02n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/02d@2x.png')
+    }
+    else if (city.weather[0].icon == '03d' || city.weather[0].icon == '03n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/03d@2x.png')
+    }
+    else if (city.weather[0].icon == '04d' || city.weather[0].icon == '04n') {
+        weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/04d@2x.png')
+    }
+
 }
